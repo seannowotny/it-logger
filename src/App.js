e3608ebc2e7875a10a12d3b2a1bcac103ed0d86e
seannowotny <seannowotny@live.de>
2019-10-21 16:25:36 +0800

    Display search bar and logs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, Fragment } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -13,9 +15,12 @@ const App = () =>
   });
 
   return (
-    <div className="App">
-      MyApp
-    </div>
+    <Fragment>
+      <SearchBar/>
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
